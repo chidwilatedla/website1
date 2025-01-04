414a8e6aac51341df528b6c1e4586b73d089596e
chidwila <chidwilatedla@gmail.com.com>
2025-01-04 10:44:15 +0530

    first version

--- let.js
+++ let.js
@@ -1,6 +1,9 @@
-var a=10;
+let age = 25; 
+console.log("Original Age :", age);
+age = 30;
+console.log("Updated Age :", age);
 {
-    let a=20;
-    console.log(a);
+  let age = 35; 
+  console.log("Age :", age);
 }
-console.log(a);
+console.log("Age outside block :", age);
